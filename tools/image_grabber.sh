09d0a98faaabe36b1ab2dda0fade895d800939b7
Karen Ouyang <kouyang@princeton.edu>
2015-02-07 18:25:21 -0500

    Improved image grabber script

--- tools/image_grabber.sh
+++ tools/image_grabber.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
-dir="../res"
+#!/bin/bash
+if [ $# -lt 2 ]; then
+  echo Usage: `basename $0` targetdir studentlist 1>&2
+  exit 1
+fi
+targetdir=$1
+students=$2
 while read line; do
   if [ -n "${line}" ]; then
-    info=`grep "$line" ../res/students.txt`
+    info=`grep "\b${line}\b" $students`
     netid=`echo "$info" | awk -F $'\t' '{print $1}'`
     url=`echo "$info" | awk -F $'\t' '{print $3}'`
-    wget -c -O "${dir}/${netid}.png" "$url"
+    wget -c -O "${targetdir}${netid}.png" "$url"
     sleep 0.5
   fi
 done
